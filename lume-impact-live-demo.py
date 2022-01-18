8252798dedc46704ed4aa3361f794c06ff96374c
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-01-18 12:22:42 -0800

    Check in corrected config

--- lume-impact-live-demo.py
+++ lume-impact-live-demo.py
@@ -115,47 +115,47 @@ if not WORKDIR:
     raise ValueError("workdir not defined in toml.")
 
 
-HOST = os.environ.get('host') # mcc-simul or 'sdf'
+HOST = config.get('host') # mcc-simul or 'sdf'
 if not HOST:
     raise ValueError("host not defined in toml.")
 
     
-IMPACT_CONFIG_FILE=os.environ.get('config_file')
+IMPACT_CONFIG_FILE=config.get('config_file')
 if not IMPACT_CONFIG_FILE:
     raise ValueError("config_file not defined in toml.")
 
-DISTGEN_INPUT_FILE=os.environ.get('distgen_input_file')
+DISTGEN_INPUT_FILE=config.get('distgen_input_file')
 if not DISTGEN_INPUT_FILE:
     raise ValueError("distgen_input_file not defined in toml.")
     
 # Directory for summary output
-SUMMARY_OUTPUT_DIR = os.environ.get('summary_output_dir')
+SUMMARY_OUTPUT_DIR = config.get('summary_output_dir')
 if not SUMMARY_OUTPUT_DIR:
     raise ValueError("summary_output_dir not defined in toml.")
 
 # Directory to output plots
-PLOT_OUTPUT_DIR = os.environ.get('plot_output_dir')
+PLOT_OUTPUT_DIR = config.get('plot_output_dir')
 if not PLOT_OUTPUT_DIR:
     raise ValueError("plot_output_dir not defined in toml.")
 
 
 # Directory for archive files
-ARCHIVE_DIR = os.environ.get('archive_dir')
+ARCHIVE_DIR = config.get('archive_dir')
 if not ARCHIVE_DIR:
     raise ValueError("archive_dir not defined in toml.")
 
 # Directory for EPICS snapshot files
-SNAPSHOT_DIR = os.environ.get('snapshot_dir')
+SNAPSHOT_DIR = config.get('snapshot_dir')
 if not SNAPSHOT_DIR:
     raise ValueError("snapshot_dir not defined in toml.")
 
 # Dummy file for distgen
-DISTGEN_LASER_FILE = os.environ.get('distgen_laser_file')
+DISTGEN_LASER_FILE = config.get('distgen_laser_file')
 if not DISTGEN_LASER_FILE:
     raise ValueError("distgen_laser_file not defined in toml.")
 
 # Number of processors
-NUM_PROCS = os.environ.get('num_procs')
+NUM_PROCS = config.get('num_procs')
 if not NUM_PROCS:
     raise ValueError("num_procs not defined in toml.")
 else:
@@ -165,17 +165,17 @@ else:
 # if using sdf:
 if HOST == 'sdf':    
     # check that environment variables are configured for execution
-    IMPACT_COMMAND = os.environ.get("impact_command")
+    IMPACT_COMMAND = config.get("impact_command")
     if not IMPACT_COMMAND:
         raise ValueError("impact_command not defined in toml.")
 
 
-    IMPACT_COMMAND_MPI = os.environ.get("impact_command_mpi")
+    IMPACT_COMMAND_MPI = config.get("impact_command_mpi")
     if not IMPACT_COMMAND_MPI:
         raise ValueError("impact_command_mpi not defined in toml.")
 
 
-MPI_RUN_CMD = os.environ.get("mpi_run_cmd")
+MPI_RUN_CMD = config.get("mpi_run_cmd")
 if not MPI_RUN_CMD:
     raise ValueError("mpi_run_cmd not defined in toml.")
 
